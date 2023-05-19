39d27e6bbc3b50d8ecda18aacf074fb9e2470610
samssjubair <samssjubair@gmail.com>
2023-05-20 00:46:37 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express= require('express')
+const app=express();
+const cors=require('cors')
+const mongoose = require('mongoose');
+
+app.use(express.json());
+app.use(cors());
+
+// schema design
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  
+});
+
+module.exports =app;
---
+++ readme.md
@@ -0,0 +1,8 @@
+Halal-Explore Backend
+
+to run the backend, run the following command in the terminal:
+
+```
+npm install
+npm start
+```
