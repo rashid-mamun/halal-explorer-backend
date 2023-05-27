83d983b64c92692cb18bf78388f155162d77daa7
rashid-mamun <rashid@sharetrip.net>
2023-05-27 22:21:27 +0600

    enable cors policy

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
+// middleware
+app.use(cors());
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ limit: '500mb' }));
 
