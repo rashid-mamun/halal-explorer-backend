0309ef6b933b9eed2b20146061511fd8a4e98cc7
rashid-mamun <rashid@sharetrip.net>
2023-06-02 09:19:32 +0600

    halal search response data update

--- src/services/halalSearch.js
+++ src/services/halalSearch.js
@@ -16,14 +16,18 @@ const searchHalalHotels = async (req) => {
     const projection = { id: 1 };
     const cursor = collection.find(query, projection);
 
-    const hotels = await cursor.toArray();
+    const dumbsHotelData = await cursor.toArray();
    
-    const hotelIds = hotels.map((hotel) => hotel.id);
+    // const hotelIds = hotels.map((hotel) => hotel.id);
     // console.log(JSON.stringify(hotelIds,null,2));
+    const hotelsDataMapping = await getHotelsDataMapping(dumbsHotelData);
+    let hotels = Object.values(hotelsDataMapping);
+    console.log(JSON.stringify(hotels));
+
     const page = req.page;
     const pageNumber = parseInt(page, 10) || 1;
     const pageSize = parseInt(req.pageSize, 10) || 20;
-    const totalHotels = hotelIds.length;
+    const totalHotels = hotels.length;
 
     // Validate page number
     const maxPageNumber = Math.ceil(totalHotels / pageSize);
@@ -37,7 +41,7 @@ const searchHalalHotels = async (req) => {
     // Calculate the offset and limit
     const offset = (pageNumber - 1) * pageSize;
     const limit = pageSize;
-    const paginatedData = hotelIds.slice(offset, offset + limit);
+    const paginatedData = hotels.slice(offset, offset + limit);
 
     return {
       success: true,
@@ -45,12 +49,42 @@ const searchHalalHotels = async (req) => {
       data: paginatedData,
     };
   } catch (err) {
+    console.log(err);
     return {
       success: false,
       error: 'Internal server error',
     };
   }
 };
+const getHotelsDataMapping = async (cursor) => {
+  const hotelsDataMapping = {};
+
+  await cursor.forEach((doc) => {
+      const hotelData = mapHotelData(doc);
+      if (hotelData.images.length > 0) {
+          hotelsDataMapping[doc.id] = hotelData;
+      }
+  });
+
+  return hotelsDataMapping;
+};
+const mapHotelData = (doc) => ({
+  hotelName: doc.name,
+  address: doc.address,
+  id: doc.id,
+  latitude: doc.latitude,
+  longitude: doc.longitude,
+  region: doc.region,
+  images: doc.images.length > 0 ? transformImageUrl(doc.images[0], '1024x768') : [],
+});
+
+const transformImageUrl = (imageUrl, size) => {
+  if (imageUrl) {
+    return imageUrl.replace('{size}', size);
+  }
+  return null; // or any other value that suits your needs
+};
+
 const createAddressIndexIfNotExists = async (collection) => {
     const indexExists = await collection.indexExists('address_text');
     if (!indexExists) {
