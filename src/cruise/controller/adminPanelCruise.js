4dd8b5591b52f0fe162e3682c74f940312bf34b0
rashid-mamun <rashid@sharetrip.net>
2023-08-02 20:12:01 +0600

    fixed bug

--- src/cruise/controller/adminPanelCruise.js
+++ src/cruise/controller/adminPanelCruise.js
@@ -2,55 +2,97 @@ const Joi = require('joi');
 const { createCruisePackage, getAllCruisePackages } = require('../services/adminPanelCruise');
 
 // Define Joi schema for validation
+// const cruisePackageSchema = Joi.object({
+//     destination: Joi.string().required(),
+//     cruiseLine: Joi.string().required(),
+//     ship: Joi.array().items(
+//         Joi.object().keys({
+//             amaWaterways: Joi.string().allow(''),
+//             azamara: Joi.string().allow(''),
+//             carnivalCruiseLines: Joi.string().allow(''),
+//             celebrityCruises: Joi.string().allow(''),
+//             costaCruises: Joi.string().allow(''),
+//             crystal: Joi.string().allow(''),
+//             disneyCruiseLine: Joi.string().allow(''),
+//             hollandAmerica: Joi.string().allow(''),
+//             hurtigrutenExpeditions: Joi.string().allow(''),
+//             hurtigrutenNorwegianCoastalExpress: Joi.string().allow(''),
+//             mscCruises: Joi.string().allow(''),
+//             norwegianCruiseLine: Joi.string().allow(''),
+//             regentSevenSeas: Joi.string().allow(''),
+//             royalCaribbean: Joi.string().allow(''),
+//             seabourn: Joi.string().allow(''),
+//             uniworldBoutiqueRiverCruises: Joi.string().allow(''),
+//             vikingOcean: Joi.string().allow(''),
+//             vikingRiver: Joi.string().allow(''),
+//             windstar: Joi.string().allow('')
+//         })
+//     ),
+//     sailingDates: Joi.array().items(Joi.string().required()).required(),
+//     length: Joi.string().required(),
+//     commentForLength: Joi.string(),
+//     itinerary: Joi.array().items(
+//         Joi.object().required()
+//     ),
+//     gallery: Joi.array().items(Joi.string().required()),
+//     shipFacts: Joi.array().items(
+//         Joi.object().required()
+//     ),
+//     shipInfo: Joi.array().items(
+//         Joi.object().required()
+//     ),
+//     policies: Joi.array().items(
+//         Joi.object().required()
+//     ),
+//     roomTypes: Joi.array().items(
+//         Joi.object().required()
+//     ),
+//     price: Joi.array().items(
+//         Joi.object().required()
+//     )
+// });
 const cruisePackageSchema = Joi.object({
     destination: Joi.string().required(),
     cruiseLine: Joi.string().required(),
-    ship: Joi.array().items(
-        Joi.object().keys({
-            amaWaterways: Joi.string().allow(''),
-            azamara: Joi.string().allow(''),
-            carnivalCruiseLines: Joi.string().allow(''),
-            celebrityCruises: Joi.string().allow(''),
-            costaCruises: Joi.string().allow(''),
-            crystal: Joi.string().allow(''),
-            disneyCruiseLine: Joi.string().allow(''),
-            hollandAmerica: Joi.string().allow(''),
-            hurtigrutenExpeditions: Joi.string().allow(''),
-            hurtigrutenNorwegianCoastalExpress: Joi.string().allow(''),
-            mscCruises: Joi.string().allow(''),
-            norwegianCruiseLine: Joi.string().allow(''),
-            regentSevenSeas: Joi.string().allow(''),
-            royalCaribbean: Joi.string().allow(''),
-            seabourn: Joi.string().allow(''),
-            uniworldBoutiqueRiverCruises: Joi.string().allow(''),
-            vikingOcean: Joi.string().allow(''),
-            vikingRiver: Joi.string().allow(''),
-            windstar: Joi.string().allow('')
-        })
-    ),
-    sailingDates: Joi.array().items(Joi.string().required()).required(),
+    ship: Joi.string().allow(''),
+    sailingDates: Joi.array().items(Joi.string().isoDate()).required(),
     length: Joi.string().required(),
     commentForLength: Joi.string(),
     itinerary: Joi.array().items(
-        Joi.object().required()
+      Joi.object({
+        key: Joi.string().required(),
+        value: Joi.string().required(),
+      })
     ),
-    gallery: Joi.array().items(Joi.string().required()),
     shipFacts: Joi.array().items(
-        Joi.object().required()
+      Joi.object({
+        key: Joi.string().required(),
+        value: Joi.string().required(),
+      })
     ),
     shipInfo: Joi.array().items(
-        Joi.object().required()
+      Joi.object({
+        key: Joi.string().required(),
+        value: Joi.string().required(),
+      })
     ),
     policies: Joi.array().items(
-        Joi.object().required()
+      Joi.object({
+        key: Joi.string().required(),
+        value: Joi.string().required(),
+      })
     ),
     roomTypes: Joi.array().items(
-        Joi.object().required()
+      Joi.object({
+        key: Joi.string().required(),
+        value: Joi.string().required(),
+      })
     ),
-    price: Joi.array().items(
-        Joi.object().required()
-    )
-});
+    price: Joi.object({
+      startsFrom: Joi.string().required(),
+    }),
+    gallery: Joi.array().items(Joi.string().required()),
+  });
 
 async function createCruisePackageController(req, res) {
     try {
