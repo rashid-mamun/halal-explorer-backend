75caaff77e6a3ee8869a73c9e9a39b0bc0d26476
rashid-mamun <rashid@sharetrip.net>
2023-09-29 18:28:15 +0600

    email demo add

--- src/auth-system/controllers/auth.js
+++ src/auth-system/controllers/auth.js
@@ -3,6 +3,43 @@ const { createUser, getUserByEmail, updateUser } = require('../models/user');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const { isEmail } = require('validator');
+const nodemailer = require('nodemailer');
+
+// Set up email transporter
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'your_email@gmail.com',
+        pass: 'your_password'
+    }
+});
+
+
+
+async function register(req, res) {
+    try {
+        const { error } = signupSchema.validate(req.body);
+        if (error) throw new Error(error.details[0].message);
+
+        const email = req.body.email;
+        const password = req.body.password;
+
+
+        const mailOptions = {
+            from: 'your_email@gmail.com', // Replace with your Gmail email address
+            to: email,
+            subject: 'Registration Complete',
+            text: `You have successfully registered. Email: ${email}, Password: ${password}`
+        };
+
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+}
+
+
 
 const signupSchema = Joi.object({
     email: Joi.string().email().required(),
@@ -88,13 +125,18 @@ async function register(req, res) {
         if (!isEmail(email)) {
             return res.status(400).json({ message: 'Invalid email format' });
         }
-
-    const existingUser = await getUserByEmail(email);
-    if (existingUser) {
-        return res.status(400).json({
-            success: false,
-            message: 'Email already registered'
-        }); // Update the user's role to "admin"
+        const mailOptions = {
+            from: 'your_email@gmail.com', // Replace with your Gmail email address
+            to: email,
+            subject: 'Registration Complete',
+            text: `You have successfully registered. Email: ${email}, Password: ${password}`
+        };
+        const existingUser = await getUserByEmail(email);
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: 'Email already registered'
+            }); // Update the user's role to "admin"
         }
         const user = await createUser(email, password, role, managerInfo);
         if (!user.success) {
@@ -102,11 +144,22 @@ async function register(req, res) {
                 success: false,
                 message: 'User creation failed'
             });
-    }
-res.status(201).json({
-            success: true,
-            message: 'User created successfully'
+        }
+         // Send the email
+         transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Email could not be sent due to a problem.' });
+            } else {
+                console.log('Email sent successfully: ' + info.response); 
+
+                res.status(201).json({ success: true, message: 'User created and email sent successfully' });
+            }
         });
+        // res.status(201).json({
+        //     success: true,
+        //     message: 'User created successfully'
+        // });
     } catch (error) {
         console.error(error);
         res.status(500).json({
